f9c77d2bfc17a793de3a67dbf101fcbb884fa8be
Mark <mark1mcerlean@gmail.com>
2021-10-31 22:46:31 +0000

    Form WIP
    Form data needs to go into div - div6 shows up but is blank

--- js/script.js
+++ js/script.js
@@ -1,19 +1,66 @@
-(document).ready(function(){ 
-    var currentMenu = 1;  
-    var curMotion = 45; 
+$(document).ready(function(){
+    // $(".gen_record").toggle();
+    // alert("Please click on the background to display items from the leaderboard.");
+    
+    $("body").click(function(){
+         $("#first_place").animate({opacity: "100%" } ,500,  function() {
+            $("body").click(function(){
+                $("#second_place").animate({opacity: "80%"}, 500 , function() {
+                    $("body").click(function(){
+                        $("#third_place").animate({opacity: "80%"} ,500 , function() {
+                            $("body").click(function(){
+                                    $("#fourth_place").animate({opacity: "80%"}, 500, function() {
+                                        $("body").click(function() {
+                                                $("#fifth_place").animate({opacity: "80%"} ,500, function(){
+                                                    $("body").click(function(){
+                                                        $("#sixth_place").animate({opacity: "80%"} ,500)
+                                                    });
+                                                });
+                                        });
+                                    }); 
+                                });
+                            })
+                        });
+                });
+        });
+    });
+});
 
-    //  Animate a row of the data and use the on so that it is dynamic for newly added row information from the 
-FORM 
-    $(".menuOuter").on("click",function(){ 
-        $("#li"+currentMenu).animate({left:  curMotion + '%' }); 
-        currentMenu += 1; 
-        curMotion -= 3;  
-    }); 
- 
-    $(".error").hide();     // make the error messages invisible on page load 
- 
-       
-    $('#submit').click(function(evt) { 
-        evt.stopImmediatePropagation();               // stop the natural HTML FORM submit 
-        error = false;                                 // assume there are no errors and then prepare to check 
+    $(".error").hide();
+
+    $('#myForm').on('submit',function(evt){
+        evt.preventDefault();
+        error=false;
         $(".error").hide();
+
+        var score = $("#score").val();
+        var name = $("#lb_name").val();
+        // 
+        if(name.length < 4)
+        {
+            $("#err_name").show();
+            error=true;
+        }
+        if  (score < 0 || score > 99)
+        {
+            $("#err_score").show();
+            error=true;
+        }
+        if (error)
+        alert("There is a problem! sorry!");
+        else{
+            $("#newInfo").hide();
+            console.log("Data is being added" + score);
+            item = $('div.gen_record').length+1;
+            console.log("DIV Length: " + item);
+            $(".container").append('<div class="gen_record" id="sixth_place"' /* +item */ + name + " " + score +'</div>');
+        };
+        
+
+        $("#addInfo").click(function(event){
+            event.stopImmediatePropagation();
+            $("#newInfo").toggle(1000);
+            $("#lb_name").focus();
+        });
+    });
+});
